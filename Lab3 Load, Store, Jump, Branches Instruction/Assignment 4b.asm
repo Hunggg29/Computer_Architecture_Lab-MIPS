63cb0e824c098c82b681e14288548b0aabc94308
Pham Duy Hung <141256670+Hunggg29@users.noreply.github.com>
2024-04-19 01:57:10 +0700

    Update Assignment 4b.asm

---
+++ Lab3 Load, Store, Jump, Branches Instruction/Assignment 4b.asm
@@ -0,0 +1,19 @@
+start: 
+
+li $s1,9 #i=9 
+
+li $s2,2 #j=2 
+
+bge $t0,$zero,else # branch to else if i>=j 
+
+addi $t1,$t1,1 # then part: x=x+1 
+
+addi $t3,$zero,1 # z=1 
+
+j endif # skip “else” part 
+
+else: 	addi $t2,$t2,-1 # begin else part: y=y-1 
+
+add $t3,$t3,$t3 # z=2*z 
+
+endif: 
